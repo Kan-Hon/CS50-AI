f6c0c4421115e88354151e025582af41e39d7684
Kan-Hon <66121721+Kan-Hon@users.noreply.github.com>
2021-08-22 22:09:43 +0800

    Add files via upload

---
+++ minesweeper/test.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jun  1 23:28:41 2020
+
+@author: kan_h
+"""
+
+subset1 = {1,2,3,4}
+
+subset2 = {1,2,3,4}
+
+print(subset2.issubset(subset1))
+
+print(subset2 == subset1)
